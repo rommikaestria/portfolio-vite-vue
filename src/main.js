73cf2f9bdc9f239654ea56e1d7c5d40e2516b60b
mazrommi <rokafordev@gmail.com>
2025-05-18 14:17:48 +0700

    Ganti Bootstrap menjadi PrimeVue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import './style.css';
 import App from './App.vue';
 import router from './router/index.js';
 
-//import Bootstrap CSS dan Js
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import PrimeVue
+import PrimeVue from 'primevue/config';
+
+// Import tema PrimeVue
+import 'primevue/resources/themes/lara-light-blue/theme.css'; //theme
+
+// Import PrimeVue Core CSS
+import 'primevue/resources/primevue.min.css'; //core css
+
+// Import PrimeIcons
+import 'primeicons/primeicons.css'; //icons
+
+// Import PrimeFlex
+import 'primeflex/primeflex.css'; //flex utilities
 
 createApp(App).use(router).mount('#app');
