d569ad1bfee782a2a312ffc18f6ec57172a2200a
mazrommi <rokafordev@gmail.com>
2025-05-18 14:53:41 +0700

    ubah routes menu halaman

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
-// Import komponen untuk setiap halaman
 import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import Contact from '../views/Contact.vue';
-import Projects from '../views/Projects.vue';
 
-// Definisikan rune
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
-  { path: '/projects', name: 'Projects', component: Projects },
-  { path: '/contact', name: 'Contact', component: Contact },
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import('../views/Projects.vue'),
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/Contact.vue'),
+  },
 ];
 
-//Buat router
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
-// Ekspor router
 export default router;
